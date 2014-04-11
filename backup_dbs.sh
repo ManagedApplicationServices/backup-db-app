af168b3c89bfc7937f32d57f310661ac110e2cb7
Sam Hon <coffeedaemon@gmail.com>
2014-04-11 15:33:13 +0800

    Added mongodb backup option

--- backup_dbs.sh
+++ backup_dbs.sh
@@ -3,16 +3,22 @@
 src=`pwd`
 fdate=`date +%Y-%b-%d`
 
-for dir in `ls $src/`
+# `ls $src`
+for dir in `ls $src`
 do
   if [ -d "$src/$dir" ]; then
-    echo Backing up $dir ...
+    echo backing up $dir ...
     cd $dir
-    db=`cat .db`
-    pg_dump -h localhost -U postgres -Fc --no-acl --no-owner $db -f $fdate-$db.dump.psql 
+    if [ -f .db ]; then
+      db=`cat .db`
+      pg_dump -h localhost -U postgres -Fc --no-acl --no-owner $db -f $fdate-$db.dump.psql
+    fi
+    if [ -f .mongodb ]; then
+      db=`cat .mongodb`
+      mongodump -h 10.0.0.85 --out ./ --db $db
+      tar zcvf $fdate-$db.mongodb.tgz ./$db
+      rm -rf ./$db
+    fi
     cd $src
   fi
 done
-echo Completed backing up all databases.
-
-
